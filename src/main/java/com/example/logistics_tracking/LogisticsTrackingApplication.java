d58dd13bb385a2db85b1628438ea27ad43e36c08
LUC-XAVIER <foningxavier@gmail.com>
2026-03-29 15:04:12 +0100

    creating spring project

---
+++ src/main/java/com/example/logistics_tracking/LogisticsTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.example.logistics_tracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsTrackingApplication.class, args);
+	}
+
+}
