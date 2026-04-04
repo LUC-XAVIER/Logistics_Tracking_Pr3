19331ede8f7473c6c1f06d3fef0d9fe3149c35a1
Abena Alex <alex.nelson.bryan@gmail.com>
2026-04-04 17:33:15 +0100

    chore: change the properties files to yaml, enable feign on business services and configure gateway to allow all requests

--- ParcelService/src/main/java/com/example/logistics_tracking/LogisticsTrackingApplication.java
+++ ParcelService/src/main/java/com/example/logistics_tracking/LogisticsTrackingApplication.java
@@ -2,8 +2,10 @@ package com.example.logistics_tracking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LogisticsTrackingApplication {
 
 	public static void main(String[] args) {
