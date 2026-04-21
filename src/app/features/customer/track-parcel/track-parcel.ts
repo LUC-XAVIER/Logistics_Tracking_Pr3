5bcb2b0ffbadec4fc202bd60007ceaecf29ff36d
cheugs <88487663+cheugs@users.noreply.github.com>
2026-04-21 20:17:45 +0100

    initial commit

---
+++ src/app/features/customer/track-parcel/track-parcel.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-track-parcel',
+  imports: [],
+  templateUrl: './track-parcel.html',
+  styleUrl: './track-parcel.css',
+})
+export class TrackParcel {}
