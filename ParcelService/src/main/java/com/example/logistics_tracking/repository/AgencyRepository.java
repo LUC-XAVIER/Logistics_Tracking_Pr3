aafc2512df2ec6f927dc8b2db5e330619cde6194
Weber <fotsingweber4@gmail.com>
2026-04-21 14:00:09 +0200

    Feat(parcel service): implemented the features of the app linked to the parcel creation and publishing of events

--- ParcelService/src/main/java/com/example/logistics_tracking/repository/AgencyRepository.java
+++ ParcelService/src/main/java/com/example/logistics_tracking/repository/AgencyRepository.java
@@ -3,6 +3,7 @@ package com.example.logistics_tracking.repository;
 import com.example.logistics_tracking.entity.Agency;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.UUID;
 
@@ -10,5 +11,8 @@ import java.util.UUID;
 public interface AgencyRepository extends JpaRepository<Agency, UUID> {
 
     List<Agency> findByCountry(String country);
+
     List<Agency> findByCountryAndTown(String country, String town);
+
+    List<Agency> findByTown(String town);
 }
--- ParcelService/src/main/java/com/example/logistics_tracking/repository/ParcelRepository.java
+++ ParcelService/src/main/java/com/example/logistics_tracking/repository/ParcelRepository.java
@@ -4,11 +4,15 @@ import com.example.logistics_tracking.entity.Parcel;
 import com.example.logistics_tracking.enums.ParcelStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
 @Repository
 public interface ParcelRepository extends JpaRepository<Parcel, String> {
 
     List<Parcel> findByUserId(String userId);
+
     List<Parcel> findByStatus(ParcelStatus status);
+
+    List<Parcel> findByUserIdAndStatus(String userId, ParcelStatus status);
 }
