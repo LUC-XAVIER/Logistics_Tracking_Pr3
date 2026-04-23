ec752abdbcad8c184c43cfb81a5b3f13888da080
Weber <fotsingweber4@gmail.com>
2026-04-23 10:15:55 +0200

    fix(controllers): updated controllers to add /logistics in front of all endpoints

--- ParcelService/src/main/java/com/example/logistics_tracking/controller/ParcelController.java
+++ ParcelService/src/main/java/com/example/logistics_tracking/controller/ParcelController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/parcels")
+@RequestMapping("/logistics/api/v1/parcels")
 @RequiredArgsConstructor
 public class ParcelController {
 
--- ParcelService/src/main/java/com/example/logistics_tracking/controller/RouteCacheController.java
+++ ParcelService/src/main/java/com/example/logistics_tracking/controller/RouteCacheController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/routes")
+@RequestMapping("/logistics/api/v1/routes")
 public class RouteCacheController {
 
     private final RouteCacheRepository routeCacheRepository;
